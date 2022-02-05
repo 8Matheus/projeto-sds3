d96467b7eb7e75e82637db4156d8b1bd21fcc160
Matheus <17matheus.senai@gmail.com>
2022-02-05 13:30:25 -0300

    LastVersion

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -4,36 +4,27 @@ import DonutChart from "components/DonutChart";
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
 
-
-
-
 const Dashboard = () => {
     return (
         <>
             <NavBar />
             <div className="container">
-
-                <h1 className="text-primary py-3">Dashboard de Vendas!</h1>
+                <h1 className="text-primary py-3">Dashboard de Vendas</h1>
 
                 <div className="row px-3">
-
                     <div className="col-sm-6">
-                        <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
+                        <h5 className="text-center text-secondary">Taxa de Sucesso(%)</h5>
                         <BarChart />
                     </div>
-
                     <div className="col-sm-6">
-                        <h5 className="text-center text-secondary">Todas Vendas</h5>
+                        <h5 className="text-center text-secondary">Todas vendas</h5>
                         <DonutChart />
                     </div>
-
-
                 </div>
 
                 <div className="py-3">
-                    <h2 className="text-primary">Total de Vendas</h2>
+                    <h2 className="text-primary">Todas Vendas</h2>
                 </div>
-
 
                 <DataTable />
             </div>
@@ -41,5 +32,4 @@ const Dashboard = () => {
         </>
     );
 }
-
 export default Dashboard;
